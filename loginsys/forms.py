7cb9b23566b041ae5b14c59e5db4c6b828e608e3
fedniko <fednik2011@gmail.com>
2018-03-26 15:40:24 +0900

    Custom UserCreationForm

---
+++ loginsys/forms.py
@@ -0,0 +1,61 @@
+from django import forms
+from django.contrib.auth import password_validation
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.forms import UserCreationForm
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    error_messages = {
+        'password_mismatch': _("Пароли не совпадают."),
+    }
+    password1 = forms.CharField(
+        label=_("Пароль"),
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Подтверждение пароля"),
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text=_("Введите тот этот же пароль для подтверждения"),
+    )
+
+    class Meta:
+        model = User
+        labels = {
+            "username": "Логин",
+            "first_name": "Имя",
+            "last_name": "Фамилия",
+            "password1": "Пароль",
+            "password2": "Подтверждение пароля"
+        }
+        fields = ('username',
+                  'first_name',
+                  'last_name',
+                  'email',
+                  'password1',
+                  'password2')
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+        return user
+
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+
+
