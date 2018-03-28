85ff30b00d37b71a0186a1be4eadc66ca9f4aa98
tyigy <tyigy25@gmail.com>
2018-03-28 15:51:43 +0900

    teachers list

--- umo/views.py
+++ umo/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import requires_csrf_token
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Person, Teacher, Student, GroupList
+
 
 # Create your views here.
 class TeacherCreate(CreateView):
     model = Person, Teacher
     fields = '__all__'
+
 
 class TeacherUpdate(UpdateView):
     model = Person, Teacher
@@ -20,8 +23,16 @@ def list_teachers(request):
     all = Teacher.objects.all()
     return render(request,'teachers_list.html', {'teachers':all})
 
+@requires_csrf_token
 def create_teacher(request):
+    arg = {}
+    #arg['form']
+    if request.method == 'POST':
+        FIO = request.POST.get('FIO', '')
+        Position = request.POST.get('Position', '')
+        Zvanie = request.POST.get('Zvanie', '')
     return render(request, 'teacher_form.html')
+
 
 class StudentCreate(CreateView):
     model = Person, Student
