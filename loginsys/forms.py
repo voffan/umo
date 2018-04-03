8e6ce3d40d94ba92ffcd850c9c12a0b47c0b96e7
fedniko <fednik2011@gmail.com>
2018-04-03 12:49:10 +0900

    footer

--- loginsys/forms.py
+++ loginsys/forms.py
@@ -1,37 +1,12 @@
 from django import forms
-from django.contrib.auth import password_validation
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.forms import UserCreationForm
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
-    error_messages = {
-        'password_mismatch': _("Пароли не совпадают."),
-    }
-    password1 = forms.CharField(
-        label=_("Пароль"),
-        strip=False,
-        widget=forms.PasswordInput,
-        help_text=password_validation.password_validators_help_text_html(),
-    )
-    password2 = forms.CharField(
-        label=_("Подтверждение пароля"),
-        widget=forms.PasswordInput,
-        strip=False,
-        help_text=_("Введите этот же пароль для подтверждения"),
-    )
-
     class Meta:
         model = User
-        labels = {
-            "username": "Логин",
-            "first_name": "Имя",
-            "last_name": "Фамилия",
-            "password1": "Пароль",
-            "password2": "Подтверждение пароля"
-        }
         fields = ('username',
                   'first_name',
                   'last_name',
@@ -58,7 +33,7 @@ class RegistrationForm(UserCreationForm):
         self.fields['first_name'].widget.attrs['class'] = 'form-control'
         self.fields['last_name'].widget.attrs['class'] = 'form-control'
         self.fields['email'].widget.attrs['class'] = 'form-control'
-        for fieldname in ['username', 'password1', 'password2']:
-            self.fields[fieldname].help_text = None
+        # for fieldname in ['username', 'password1', 'password2']:
+        #     self.fields[fieldname].help_text = None
 
 
