a5b8e3292d88a10bbef4e322a5ffc91941364dc7
fedniko <fednik2011@gmail.com>
2018-04-07 13:46:48 +0900

    disciplines: (Reverse) и мелкие исправления

--- umo_project/urls.py
+++ umo_project/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^student/$', views.StudentListView.as_view(), name='student_changelist'),
     url(r'^student/add/$', views.StudentCreateView.as_view(), name='student_add'),
     url(r'^student/delete/$', views.student_delete, name = 'student_delete'),
-    url(r'^disciplines/', include('disciplines.urls')),
+    url(r'^disciplines/', include('disciplines.urls', namespace='disciplines')),
     url(r'^$', include('loginsys.urls')),
 ]
