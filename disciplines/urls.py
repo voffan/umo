aafcd4f44e546a73ce284a85a538f3c508e2709c
fedniko <fednik2011@gmail.com>
2018-05-02 00:42:45 +0900

    вывод .xls таблицы экран успеваемости c помощью openpyxl (сделайте в cmd 'pip install openpyxl')

--- disciplines/urls.py
+++ disciplines/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
-from disciplines import views
-from django.contrib.auth.decorators import login_required
 
+from disciplines import views
 
 urlpatterns = [
     url(r'^add/$', views.DisciplineCreate.as_view(), name='disciplines_add'),
     url(r'(?P<pk>[0-9]+)/update/$', views.DisciplineUpdate.as_view(), name='update'),
     url(r'^delete/$', views.discipline_delete, name='delete'),
+    url(r'^export/$', views.export_to_excel, name='export'),
     url(r'(?P<pk>[0-9]+)/$', views.discipline_detail, name='detail'),
     url(r'^add_details/$', views.DetailsCreate.as_view(), name='details_add'),
     url(r'^$', views.DisciplineList.as_view(), name='disciplines_list'),
