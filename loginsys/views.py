ec3dd3196d994f906bd5b0c76650c86848bcafbd
fedniko <fednik2011@gmail.com>
2018-05-15 01:26:42 +0900

    Изменение навигационной панели (разделение на преподавателя и администратора)

--- loginsys/views.py
+++ loginsys/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import redirect, render, reverse
-from django.http import HttpResponseRedirect
 from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render, reverse
+
 from loginsys.forms import RegistrationForm
 
 
@@ -12,7 +13,7 @@ def login(request):
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect(reverse('disciplines:disciplines_list'))
+            return redirect(reverse('disciplines:teacher'))
         else:
             args['login_error'] = "Внимание, вход на сайт не был произведен. " \
                                   "Возможно, вы ввели неверное имя пользователя или пароль."
