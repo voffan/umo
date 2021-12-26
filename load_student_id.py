e4c21357aba1eb36d4f3a94215f79e33d7c62399
voffan <vovan_evv@rambler.ru>
2021-12-26 23:16:45 +0900

    fixed group brs points view

--- load_student_id.py
+++ load_student_id.py
@@ -1,21 +1,31 @@
 from umo_project.wsgi import application
 from umo.models import GroupList
 
-students = open('students.csv').read().split('\n')
-students = [student.split(';') for student in students]
 
-for student in students:
-    fio = student[1] + ' ' + student[2]
-    if len(student[3]) > 0:
-        fio = fio + ' ' + student[3]
-    sl = GroupList.objects.filter(group__Name='ИМИ-'+student[0], student__FIO=fio).first()
-    if sl is not None:
-        st = sl.student
-        st.student_id = student[4]
-        st.save()
-        '''
-        if len(sl) == 1:
-            sl[0].student.student_id = student[4]
-            sl[0].student.save()
+def load_students(csv_file):
+    students = open(csv_file).read().split('\n')
+    students = [student.split(';') for student in students]
+
+    for student in students:
+        fio = student[1]
+        if len(student[2]) > 0:
+            sl = GroupList.objects.filter(group__Name=student[0], student__FIO=fio).first()
+            if sl is not None:
+                st = sl.student
+                st.student_id = student[2]
+                st.save()
+            else:
+                print(student[0], student[1])
         else:
-            print(student)'''
+            print(student[0], student[1])
+            '''
+            if len(sl) == 1:
+                sl[0].student.student_id = student[4]
+                sl[0].student.save()
+            else:
+                print(student)'''
+
+
+if __name__ == '__main__':
+    load_students('bak21.csv')
+    load_students('mag21.csv')
